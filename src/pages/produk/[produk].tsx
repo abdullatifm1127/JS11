8b0e10a8cef74c08880fcfc34c73ce332a0170e3
latif <mabdullatifm1127@gmail.com>
2026-04-08 01:24:50 +0700

    Bagian 4

--- src/pages/produk/[produk].tsx
+++ src/pages/produk/[produk].tsx
@@ -24,12 +24,36 @@ const HalamanProduk = ({product}: {product: ProductType}) => {
 
 export default HalamanProduk;
 
-export async function getServerSideProps({ params}: { params: { produk: string } }) {
-    const res = await fetch(`http://localhost:3000/api/products/${params?.produk}`);
+// export async function getServerSideProps({ params}: { params: { produk: string } }) {
+//     const res = await fetch(`http://localhost:3000/api/products/${params?.produk}`);
+//     const respone = await res.json();
+//     return {
+//         props: {
+//             products: respone.data,
+//         },
+//     };
+// }   
+
+export async function getStaticPaths() {
+    const res = await fetch(`http://localhost:3000/api/products`);
     const respone = await res.json();
+
+    const paths = respone.data.map((product: ProductType) => ({
+        params: { produk: product.id.toString() },
+    }));
+
+    return {
+        paths,
+        fallback: false,
+    };
+}
+export async function getStaticProps({ params }: { params: { produk: string } }) {
+    const res = await fetch(`http://localhost:3000/api/products/${params?.produk}`);
+    const respone = await res.json() as { data: ProductType };
+
     return {
         props: {
-            products: respone.data,
+            product: respone.data,
         },
     };
-}   
+}
